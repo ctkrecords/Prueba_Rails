679fe399fb16622684f840b8a558c59f868ec8e2
ctkrecords <ctkrecordings@gmail.com>
2019-06-28 09:41:58 -0600

    CHANGES: CSS and another functions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -47,4 +47,5 @@ class ArticlesController < ApplicationController
         def article_params
             params.require(:article).permit(:title, :text, :category)
         end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_153243) do
+ActiveRecord::Schema.define(version: 2019_06_27_235011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_153243) do
     t.string "category"
     t.string "nombre_usuario"
     t.string "correo_usuario"
+    t.integer "comments_count"
   end
 
   create_table "comments", force: :cascade do |t|
